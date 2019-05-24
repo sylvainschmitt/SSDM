72dcbf4b8bfdabc4de4fad9b9f3c1a1cd42760ab
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2019-05-24 15:05:03 +0200

    Travis-fix 1

--- R/load_occ.R
+++ R/load_occ.R
@@ -31,6 +31,7 @@ NULL
 #'  not).
 #'
 #' @examples
+#' data(Env)
 #' load_occ(path = system.file('extdata',  package = 'SSDM'), Env,
 #'          Xcol = 'LONGITUDE', Ycol = 'LATITUDE',
 #'          file = 'Occurrences.csv', sep = ',')
