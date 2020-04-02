c4c3b21d4ecb50b936208162010fd2386dfd25ba
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2020-04-02 12:53:13 +0000

    rm Maxent from test for Travis

--- tests/testthat/test_modelling.R
+++ tests/testthat/test_modelling.R
@@ -2,7 +2,7 @@ test_that('modelling function', {
   data(Env)
   data(Occurrences)
   Occurrences <- subset(Occurrences, Occurrences$SPECIES == 'elliptica')
-  available.algo <- available.algo <- c("GLM", "GAM", "MARS", "GBM", "CTA", "RF", "MAXENT","ANN", "SVM")
+  available.algo <- available.algo <- c("GLM", "GAM", "MARS", "GBM", "CTA", "RF", "ANN", "SVM")
   for(i in available.algo){
     cat(paste("testing", i, "...\n"))
     show_failure(SDM <- modelling(i, Occurrences, Env, Xcol = 'LONGITUDE', Ycol = 'LATITUDE', verbose = F))
